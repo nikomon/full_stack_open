b426150cf58aab2a9cf9be5271f8b3b177008489
Niko Niittyranta <niko.niittyranta@aller.com>
2019-12-03 17:05:27 +0200

    Change: port

--- index.js
+++ index.js
@@ -83,5 +83,7 @@ app.get('/info', (request, response) => {
 response.send(`<p>Phonebook has info for ${persons.length} people</p>
 ${new Date(Date.now())}`);
 });
-
-app.listen((process.env.PORT || 3001))
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
