7371043a715f837d3240ffb88a5669dd5225eb15
Niko Niittyranta <niko.niittyranta@aller.com>
2019-12-03 09:30:35 +0200

    Add: filter component

--- osa2/2b/src/Filter.js
+++ osa2/2b/src/Filter.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export const Filter = () => {
+export const Filter = ({ updateFilter }) => {
+    const [filterValue, setFilterValue] = useState("");
+    useEffect(() => {
+        updateFilter(filterValue.toLowerCase());
+    }, [filterValue])
     return (
-        <></>
+        <form>
+        <div>
+          Filter shown with: <input value={filterValue} onChange={(event) => setFilterValue(event.target.value)}/>
+        </div>
+        <div>
+        </div>
+      </form>
     );
 }
